a42e311c4e410959ca4e3777c020dcf6dba90e78
이영욱 <lyuk98@hotmail.com>
2024-03-12 10:58:57 +0900

    style: indent 스타일 변경
    Tab 대신 space 문자를 사용합니다

--- backend/python/main.py
+++ backend/python/main.py
@@ -6,8 +6,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-	return {"Hello": "World"}
+    return {"Hello": "World"}
 
 @app.get("items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-	return {"item_id": item_id, "q": q}
+    return {"item_id": item_id, "q": q}
