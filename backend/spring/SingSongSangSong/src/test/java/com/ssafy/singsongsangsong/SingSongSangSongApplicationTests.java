fdec2e42f28f60ee5e25942eb9fd68513d4909c3
joonamin <joonamin44@gmail.com>
2024-03-13 16:52:28 +0900

    init: initialize spring `backend` project

---
+++ backend/spring/SingSongSangSong/src/test/java/com/ssafy/singsongsangsong/SingSongSangSongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.singsongsangsong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingSongSangSongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
