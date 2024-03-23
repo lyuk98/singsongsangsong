470e3f2fe3feceed84fb2e60b03614efed1c94b4
heunoh <gms245@naver.com>
2024-03-23 17:45:23 +0900

    feat : artist page

--- frontend/src/pages/root/ArtistPage.tsx
+++ frontend/src/pages/root/ArtistPage.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { useParams } from "react-router";
+import { RiMusic2Fill } from "react-icons/ri";
+import { TbMoodNerd } from "react-icons/tb";
 
 import ArtistHeader from "../../components/pageComponents/artistpageComponent/ArtistHeader";
 import styles from "./ArtistPage.module.css";
+import EmotionBox from "../../components/public/emotionBox/EmotionBox";
+import BarChart from "../../components/public/chart/barChart/BarChart";
+import MusicTable from "../../components/public/music/MusicTable";
 
 const ArtistPage = () => {
   const { artistId } = useParams();
@@ -13,7 +18,34 @@ const ArtistPage = () => {
         <ArtistHeader />
       </div>
       <div className={styles.content}>
-        <p>내부 패딩 6rem 이다 흔오야</p>
+        <div className={`flex-row-center w-100`}>
+          <div className={"w-50"}>
+            <h2 className={`flex-row`}>
+              주요 장르&nbsp;
+              <RiMusic2Fill />
+            </h2>
+            <div style={{ height: "300px" }} className={`w-100`}>
+              <BarChart option="장르" />
+            </div>
+          </div>
+          <div className={"w-50"}>
+            <h2 className={`flex-row`}>
+              주요 분위기&nbsp;
+              <RiMusic2Fill />
+            </h2>
+            <div style={{ height: "300px" }} className={`w-100`}>
+              <BarChart option="분위기" />
+            </div>
+          </div>
+        </div>
+        <div className={`flex-col-center bg-box ${styles.emotionBox}`}>
+          <h1>사람들은 {artistId} 님에게서</h1>
+          <h1>이런 느낌을 받았어요</h1>
+          <EmotionBox />
+        </div>
+        <div className={`w-100`}>
+          <MusicTable />
+        </div>
       </div>
     </div>
   );
