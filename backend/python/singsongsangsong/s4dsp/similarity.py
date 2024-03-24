c46080da583fba02fc3e47cde505ea6d49681678
이영욱 <lyuk98@hotmail.com>
2024-03-25 02:50:35 +0900

    feat: 곡 유사도 분석 기능 초안 작성
    정확도 개선이 필요하지만 유사도 분석 구현의 방향을 설정합니다

--- backend/python/singsongsangsong/s4dsp/similarity.py
+++ backend/python/singsongsangsong/s4dsp/similarity.py
@@ -1,78 +1,140 @@
 import os
-from voyager import Index, Space
+import time
+from itertools import count, zip_longest
+from dotenv import load_dotenv
 import numpy as np
 import librosa
-import time
-import sys
+from pymilvus import MilvusClient, DataType
+
+def grouper(iterable, n, *, incomplete="fill", fillvalue=None):
+    """Collect data into non-overlapping fixed-length chunks or blocks
+
+    grouper("ABCDEFG", 3, fillvalue="x") --> ABC DEF Gxx
+    grouper("ABCDEFG", 3, incomplete="strict") --> ABC DEF ValueError
+    grouper("ABCDEFG", 3, incomplete="ignore") --> ABC DEF
+    """
+
+    args = [iter(iterable)] * n
+
+    if incomplete == "fill":
+        return zip_longest(*args, fillvalue=fillvalue)
+    if incomplete == "strict":
+        return zip(*args, strict=True)
+    if incomplete == "ignore":
+        return zip(*args)
+
+    raise ValueError("Expected fill, strict, or ignore")
+
+def load_audio(path: str, sr: float) -> np.ndarray:
+    return librosa.load(path, sr=sr, mono=True)[0]
 
 def get_mfcc(y: np.ndarray, sr: float) -> np.ndarray:
-    mfcc = librosa.feature.mfcc(y=y, sr=sr)
-
-    mean = np.mean(mfcc, axis=0)
-    covariance = np.cov(mfcc, rowvar=False)
-    
-    vector = np.concatenate([np.diag(covariance, k) for k in range(len(mean))])
-    vector = np.concatenate((mean, vector))
-    print(vector.shape)
-
-    # vector = mean
-    # for k in range(len(mean)):
-    # 	diagonal = np.diag(covariance, k)
-    # 	print(diagonal)
-    # 	print(len(diagonal))
-    # 	vector = np.concatenate((vector, diagonal))
-    
-    return vector
-
-def add_audio(reference: list[float], index: Index):
-    return index.add_item(reference[:index.num_dimensions])
+    return librosa.feature.mfcc(y=y, sr=sr, n_mfcc=1)[0]
 
 # Parameters
-reference_path = "data/train/"
-compare_path = "data/compare.wav"
-index_path = "data/index.voy"
+REFERENCE_PATH = "data/train/"
 
-sample_rate = 22050
-num_dimensions = 12582912
+SAMPLE_RATE = 22050
+SPLIT_SAMPLES = librosa.time_to_samples(10, sr=SAMPLE_RATE)
+NUM_DIMENSIONS = 431
 
-# Create index
-index = Index(Space.Euclidean, num_dimensions=num_dimensions)
+load_dotenv()
 
-references = {}
+client = MilvusClient(
+    uri=os.environ.get("MILVUS_URI")
+)
 
-with os.scandir(reference_path) as it:
+schema = MilvusClient.create_schema(
+    auto_id=True,
+    enable_dynamic_field=False
+)
+schema.add_field(field_name="id", datatype=DataType.INT64, is_primary=True)
+schema.add_field(field_name="song_name", datatype=DataType.VARCHAR, max_length=128)
+schema.add_field(field_name="order", datatype=DataType.INT32)
+schema.add_field(field_name="vector", datatype=DataType.FLOAT_VECTOR, dim=NUM_DIMENSIONS)
+
+index_parameters = client.prepare_index_params()
+index_parameters.add_index(
+    field_name="id",
+    index_type="STL_SORT"
+)
+index_parameters.add_index(
+    field_name="vector",
+    index_type="IVF_FLAT",
+    metric_type="IP",
+    params={ "nlist": 128 }
+)
+
+client.create_collection(
+    collection_name="mfcc_test",
+    schema=schema,
+    index_params=index_parameters
+)
+
+print(client.get_load_state(collection_name="mfcc_test"))
+
+with os.scandir(REFERENCE_PATH) as it:
     for entry in it:
         if entry.name.startswith(".") or not entry.is_file():
             continue
 
-        audio, _ = librosa.load(entry.path, sr=sample_rate, mono=True)
-        mfcc = get_mfcc(y=audio, sr=sample_rate)
-        print(mfcc)
-        print(len(mfcc))
+        reference_audio = load_audio(entry.path, sr=SAMPLE_RATE)
 
-        print(f"Adding {entry.name} to index")
+        entities = []
+        order = count(start=0, step=1)
+
+        for segment in grouper(reference_audio, SPLIT_SAMPLES, incomplete="fill", fillvalue=0.0):
+            entities.append(
+                {
+                    "song_name": entry.name,
+                    "order": next(order),
+                    "vector": get_mfcc(y=np.array(segment), sr=SAMPLE_RATE)
+                }
+            )
+
+        print(f"Adding {entry.name} ({len(entities)} chunks) to collection")
 
         add_start = time.perf_counter_ns()
-        audio_id = add_audio(mfcc, index)
+        client.insert(
+            collection_name="mfcc_test",
+            data=entities
+        )
         add_end = time.perf_counter_ns()
 
         print(f"Adding {entry.name} took {(add_end - add_start) // 1000000} milliseconds")
 
-        references[audio_id] = entry.name
-        # sys.exit()
+FILENAME = "data/audio.wav"
 
-audio, _ = librosa.load(compare_path, sr=sample_rate, mono=True)
-compare_mfcc = get_mfcc(y=audio, sr=sample_rate)
-del audio
+audio = load_audio(path=FILENAME, sr=SAMPLE_RATE)
 
-print(f"Querying {compare_path}")
+print("Searching similarity")
 
-query_start = time.perf_counter_ns()
-neighbours, distances = index.query(compare_mfcc[:num_dimensions], k=3)
-query_end = time.perf_counter_ns()
+search_start = time.perf_counter_ns()
+results = client.search(
+    collection_name="mfcc_test",
+    data=[
+        get_mfcc(
+            y=np.array(chunk),
+            sr=SAMPLE_RATE
+        ) for chunk in grouper(audio, SPLIT_SAMPLES, incomplete="fill", fillvalue=0.0)
+    ],
+    limit=3,
+    search_params={},
+    group_by_field="song_name",
+    output_fields=["song_name", "order"]
+)
+search_end = time.perf_counter_ns()
 
-print(f"Querying took {(query_end - query_start) // 1000000} milliseconds")
+print(f"Similarity search took {(search_end - search_start) // 1000000} milliseconds")
 
-print("Result:")
-for i in range(len(neighbours)):
-    print(f"  {references[neighbours[i]]}: {distances[i]}")
+time_count = count(0, 1)
+for result_set in results:
+    current = next(time_count)
+    print(f"Result for chunk {current} ({current * 10}-{(current + 1) * 10} seconds)")
+
+    for result in result_set:
+        print(
+            f"  {result['entity']['song_name']}"
+            f"({result['entity']['order'] * 10}-{(result['entity']['order'] + 1) * 10})"
+            f" - Distance: {result['distance']}"
+        )
