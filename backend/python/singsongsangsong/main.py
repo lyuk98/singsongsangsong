31d3f28573079d8c417d41f8731f09c240097172
이영욱 <lyuk98@hotmail.com>
2024-03-26 07:45:16 +0900

    feat: API endpoint 준비
    구현 완료 시 501이 아닌 response code를 반환합니다

--- backend/python/singsongsangsong/main.py
+++ backend/python/singsongsangsong/main.py
@@ -1,16 +1,39 @@
-from typing import Union
-from fastapi import FastAPI
+"""
+싱송생송 DSP API
+===============
+"""
+
+from typing import Annotated
+from fastapi import FastAPI, Path, Query, Response, status
 import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/song")
+def request_song_analysis(
+    song_id: Annotated[int, Query(alias="id")],
+    audio_path: Annotated[str, Query(alias="path")]
+):
+    return Response(status_code=status.HTTP_501_NOT_IMPLEMENTED)
 
-@app.get("items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/song/{id}")
+def query_progress(
+    song_id: Annotated[int, Path(alias="id")]
+):
+    return Response(status_code=status.HTTP_501_NOT_IMPLEMENTED)
+
+@app.post("/similarity")
+def save_similarity_data(
+    song_id: Annotated[int, Query(alias="id")],
+    audio_path: Annotated[str, Query(alias="path")]
+):
+    return Response(status_code=status.HTTP_501_NOT_IMPLEMENTED)
+
+@app.get("/similarity")
+def check_similarity(
+    audio_path: Annotated[str, Query(alias="path")]
+):
+    return Response(status_code=status.HTTP_501_NOT_IMPLEMENTED)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
