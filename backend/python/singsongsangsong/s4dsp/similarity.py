9002d53ba350b2064776d126ca9e2c356688b886
이영욱 <lyuk98@hotmail.com>
2024-03-27 14:59:58 +0900

    feat: 유사도 분석 기능 작성

--- backend/python/singsongsangsong/s4dsp/similarity.py
+++ backend/python/singsongsangsong/s4dsp/similarity.py
@@ -1,140 +1,142 @@
+"""
+유사도 분석
+==========
+
+`s4dsp.similarity`는 음원을 유사도를 분석할 수 있는 형태로 변환합니다
+
+Utility functions
+-----------------
+`get_embedding`
+    PANNs를 사용하여 음원의 패턴을 확인합니다
+"""
+
 import os
 import time
-from itertools import count, zip_longest
 from dotenv import load_dotenv
 import numpy as np
 import librosa
+from panns_inference import AudioTagging
 from pymilvus import MilvusClient, DataType
 
-def grouper(iterable, n, *, incomplete="fill", fillvalue=None):
-    """Collect data into non-overlapping fixed-length chunks or blocks
+def get_embedding(y: np.ndarray) -> list[float]:
+    """PANNs를 사용하여 음원의 패턴을 확인합니다
 
-    grouper("ABCDEFG", 3, fillvalue="x") --> ABC DEF Gxx
-    grouper("ABCDEFG", 3, incomplete="strict") --> ABC DEF ValueError
-    grouper("ABCDEFG", 3, incomplete="ignore") --> ABC DEF
+    Parameters
+    ----------
+    y : np.ndarray
+        모노 음원
+
+    Returns
+    -------
+    list[float]
+        embedding
     """
 
-    args = [iter(iterable)] * n
+    source = y[None, :]
 
-    if incomplete == "fill":
-        return zip_longest(*args, fillvalue=fillvalue)
-    if incomplete == "strict":
-        return zip(*args, strict=True)
-    if incomplete == "ignore":
-        return zip(*args)
+    tag = AudioTagging(checkpoint_path="s4dsp/data/models/Cnn14_mAP=0.431.pth")
+    embedding = tag.inference(source)[1]
 
-    raise ValueError("Expected fill, strict, or ignore")
+    embedding = embedding / np.linalg.norm(embedding)
 
-def load_audio(path: str, sr: float) -> np.ndarray:
-    return librosa.load(path, sr=sr, mono=True)[0]
+    return embedding.tolist()[0]
 
-def get_mfcc(y: np.ndarray, sr: float) -> np.ndarray:
-    return librosa.feature.mfcc(y=y, sr=sr, n_mfcc=1)[0]
+if __name__ == "__main__":
+    load_dotenv()
 
-# Parameters
-REFERENCE_PATH = "s4dsp/data/train/"
+    # Parameters
+    REFERENCE_PATH = "s4dsp/data/train/"
 
-SAMPLE_RATE = 22050
-SPLIT_SAMPLES = librosa.time_to_samples(10, sr=SAMPLE_RATE)
-NUM_DIMENSIONS = 431
+    client = MilvusClient(
+        uri=os.environ.get("MILVUS_URI")
+    )
 
-load_dotenv()
+    schema = MilvusClient.create_schema(
+        enable_dynamic_field=False
+    )
+    schema.add_field(
+        field_name="song_name",
+        datatype=DataType.VARCHAR,
+        max_length=128,
+        is_primary=True
+    )
+    schema.add_field(
+        field_name="vector",
+        datatype=DataType.FLOAT_VECTOR,
+        dim=2048
+    )
 
-client = MilvusClient(
-    uri=os.environ.get("MILVUS_URI")
-)
+    index_parameters = client.prepare_index_params()
+    index_parameters.add_index(
+        field_name="song_name",
+        index_type="Trie"
+    )
+    index_parameters.add_index(
+        field_name="vector",
+        index_type="IVF_FLAT",
+        metric_type="IP",
+        params={ "nlist": 16384 }
+    )
 
-schema = MilvusClient.create_schema(
-    auto_id=True,
-    enable_dynamic_field=False
-)
-schema.add_field(field_name="id", datatype=DataType.INT64, is_primary=True)
-schema.add_field(field_name="song_name", datatype=DataType.VARCHAR, max_length=128)
-schema.add_field(field_name="order", datatype=DataType.INT32)
-schema.add_field(field_name="vector", datatype=DataType.FLOAT_VECTOR, dim=NUM_DIMENSIONS)
+    client.create_collection(
+        collection_name="similarity_test",
+        schema=schema,
+        index_params=index_parameters
+    )
 
-index_parameters = client.prepare_index_params()
-index_parameters.add_index(
-    field_name="id",
-    index_type="STL_SORT"
-)
-index_parameters.add_index(
-    field_name="vector",
-    index_type="IVF_FLAT",
-    metric_type="IP",
-    params={ "nlist": 128 }
-)
+    print(client.get_load_state(collection_name="similarity_test"))
 
-client.create_collection(
-    collection_name="mfcc_test",
-    schema=schema,
-    index_params=index_parameters
-)
+    entities = []
 
-print(client.get_load_state(collection_name="mfcc_test"))
+    with os.scandir(REFERENCE_PATH) as it:
+        for entry in it:
+            if entry.name.startswith(".") or not entry.is_file():
+                continue
 
-with os.scandir(REFERENCE_PATH) as it:
-    for entry in it:
-        if entry.name.startswith(".") or not entry.is_file():
-            continue
+            print(f"Reading {entry.name}")
 
-        reference_audio = load_audio(entry.path, sr=SAMPLE_RATE)
-
-        entities = []
-        order = count(start=0, step=1)
-
-        for segment in grouper(reference_audio, SPLIT_SAMPLES, incomplete="fill", fillvalue=0.0):
             entities.append(
                 {
                     "song_name": entry.name,
-                    "order": next(order),
-                    "vector": get_mfcc(y=np.array(segment), sr=SAMPLE_RATE)
+                    "vector": get_embedding(
+                        librosa.load(entry.path, sr=22050, mono=True)[0]
+                    )
                 }
             )
 
-        print(f"Adding {entry.name} ({len(entities)} chunks) to collection")
+    print(f"Adding {len(entities)} songs to collection")
 
-        add_start = time.perf_counter_ns()
-        client.insert(
-            collection_name="mfcc_test",
-            data=entities
-        )
-        add_end = time.perf_counter_ns()
+    add_start = time.perf_counter_ns()
+    client.insert(
+        collection_name="similarity_test",
+        data=entities
+    )
+    add_end = time.perf_counter_ns()
 
-        print(f"Adding {entry.name} took {(add_end - add_start) // 1000000} milliseconds")
+    print(f"Adding took {(add_end - add_start) // 1000000} milliseconds")
 
-FILENAME = "s4dsp/data/audio.wav"
+    FILENAME = "s4dsp/data/audio.wav"
 
-audio = load_audio(path=FILENAME, sr=SAMPLE_RATE)
+    audio = librosa.load(FILENAME, sr=22050, mono=True)[0]
+    audio_embedding = get_embedding(librosa.load(FILENAME, sr=22050, mono=True)[0])
 
-print("Searching similarity")
+    print("Searching similarity")
 
-search_start = time.perf_counter_ns()
-results = client.search(
-    collection_name="mfcc_test",
-    data=[
-        get_mfcc(
-            y=np.array(chunk),
-            sr=SAMPLE_RATE
-        ) for chunk in grouper(audio, SPLIT_SAMPLES, incomplete="fill", fillvalue=0.0)
-    ],
-    limit=3,
-    search_params={},
-    group_by_field="song_name",
-    output_fields=["song_name", "order"]
-)
-search_end = time.perf_counter_ns()
+    search_start = time.perf_counter_ns()
+    results = client.search(
+        collection_name="similarity_test",
+        data=[audio_embedding],
+        limit=3,
+        search_params={ "nprobe": 32 }
+    )
+    search_end = time.perf_counter_ns()
 
-print(f"Similarity search took {(search_end - search_start) // 1000000} milliseconds")
+    print(f"Similarity search took {(search_end - search_start) // 1000000} milliseconds")
 
-time_count = count(0, 1)
-for result_set in results:
-    current = next(time_count)
-    print(f"Result for chunk {current} ({current * 10}-{(current + 1) * 10} seconds)")
-
-    for result in result_set:
+    for result in results[0]:
         print(
-            f"  {result['entity']['song_name']}"
-            f"({result['entity']['order'] * 10}-{(result['entity']['order'] + 1) * 10})"
+            f"  {result['id']}"
             f" - Distance: {result['distance']}"
         )
+
+    client.drop_collection(collection_name="similarity_test")
