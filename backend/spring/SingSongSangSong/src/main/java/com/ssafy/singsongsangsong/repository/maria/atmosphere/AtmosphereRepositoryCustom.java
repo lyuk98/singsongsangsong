788b3f0538335cccd78b7f9b81d75877e732e1d6
joonamin <joonamin44@gmail.com>
2024-03-29 00:31:59 +0900

    feat: 특정 곡의 테마를 분석 이후 결정
    * 분석 이후 fastAPI의 hook을 받아, Song Entry의 값을 초기화해줍니다.

--- backend/spring/SingSongSangSong/src/main/java/com/ssafy/singsongsangsong/repository/maria/atmosphere/AtmosphereRepositoryCustom.java
+++ backend/spring/SingSongSangSong/src/main/java/com/ssafy/singsongsangsong/repository/maria/atmosphere/AtmosphereRepositoryCustom.java
@@ -1,9 +1,12 @@
 package com.ssafy.singsongsangsong.repository.maria.atmosphere;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.ssafy.singsongsangsong.entity.Atmosphere;
 
 public interface AtmosphereRepositoryCustom {
-	public List<Atmosphere> getAtmosphereBySongId(Long songId);
+	List<Atmosphere> getAtmosphereBySongId(Long songId);
+
+	Optional<Atmosphere> getFirstAtmosphereBySongId(Long songId);
 }
