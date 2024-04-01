c3657287620771bbbd843b6c4f5d534a498ffc7f
이동호 <ghehd1125@gmail.com>
2024-04-02 03:14:35 +0900

    fix : Update batch

--- backend/spring/SingSongSangSong/src/main/java/com/ssafy/singsongsangsong/job/PlayJobConfig.java
+++ backend/spring/SingSongSangSong/src/main/java/com/ssafy/singsongsangsong/job/PlayJobConfig.java
@@ -1,10 +1,11 @@
 package com.ssafy.singsongsangsong.job;
 
 import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
-import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ExecutionContext;
@@ -13,17 +14,47 @@ import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.mapping.FieldSetMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import com.ssafy.singsongsangsong.dto.AtmosphereCountDto;
 import com.ssafy.singsongsangsong.dto.GenreCountDto;
 import com.ssafy.singsongsangsong.entity.Play;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
-public class ChunkStepConfig {
+@RequiredArgsConstructor
+public class PlayJobConfig {
+	
+	private final JobRepository jobRepository;
+	private final PlatformTransactionManager transactionManager;
+	
+	@Bean
+	public Job playJob() {
+		return new JobBuilder("playJob", jobRepository)
+				.start(jobStep())
+				.preventRestart()
+				.incrementer(new MyJobParametersIncrementer())
+				.build();
+	}
+	
+	@Bean
+	public Step jobStep() {
+		Step step = new StepBuilder("jobStep", jobRepository)
+				.listener(stepExecutionListener())
+				.<Play, Play>chunk(10, transactionManager)
+				.reader(csvFileReader())
+				.processor(new PlayProcessor())
+				.writer(new PlayWriter())
+				.build();
+		
+		return step;
+	}
 	
 	protected static class PlayFieldSetMapper implements FieldSetMapper<Play> {
 	    public Play mapFieldSet(FieldSet fieldSet) {
@@ -40,11 +71,11 @@ public class ChunkStepConfig {
 	}
 	
 	@Bean
-	@StepScope
+//	@StepScope
 	public FlatFileItemReader<Play> csvFileReader() {
 		FlatFileItemReader<Play> itemReader = new FlatFileItemReader<>();
 		itemReader.setEncoding("UTF-8");
-		itemReader.setResource(new FileSystemResource("resources/players.csv"));
+		itemReader.setResource(new FileSystemResource("src/main/resources/log_dummy.csv"));
 		itemReader.setLinesToSkip(1);
 		
 		DefaultLineMapper<Play> lineMapper = new DefaultLineMapper<>();
@@ -56,9 +87,12 @@ public class ChunkStepConfig {
 	}
 	
 	@Bean
-	@StepScope
+//	@StepScope
 	public StepExecutionListener stepExecutionListener() {
 	    return new StepExecutionListener() {
+	    	@Autowired
+	        private MongoTemplate mongoTemplate;
+	    	
 	        @Override
 	        public void beforeStep(StepExecution stepExecution) {
 	        	ExecutionContext stepExecutionContext = stepExecution.getExecutionContext();
@@ -80,30 +114,11 @@ public class ChunkStepConfig {
 
 	        @Override
 	        public ExitStatus afterStep(StepExecution stepExecution) {
-//	            log.info("[StepExecutionListener#afterStep] stepExecution is " + stepExecution.getStatus());
+	        	mongoTemplate.save(stepExecution.getExecutionContext().get("10M/g"));
+	        	
 	            return stepExecution.getExitStatus();
 	        }
 	    };
-	}
-	
-//	@Bean
-//	public MongoItemWriter<Play> mongoItemWriter(MongoOperations mongoTemplate) {
-//		return new MongoItemWriterBuilder<Play>()
-//				.collection("trend")
-//				.template(mongoTemplate)
-//				.build();
-//	}
-	
-	@Bean
-	public Step chunkStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-		Step step = new StepBuilder("chunckStep", jobRepository)
-				.listener(stepExecutionListener())
-				.<Play, Play>chunk(10, transactionManager)
-				.reader(csvFileReader())
-				.processor(new PlayProcessor())
-				.build();
-		
-		return step;
 	}
 
 }
