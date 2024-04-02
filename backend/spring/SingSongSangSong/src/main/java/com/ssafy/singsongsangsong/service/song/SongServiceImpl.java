5847da2701a6ea1b58da6458bcb3cd2cc4516585
joonamin <joonamin44@gmail.com>
2024-04-03 02:31:57 +0900

    fix: emotion update 버그 수정

--- backend/spring/SingSongSangSong/src/main/java/com/ssafy/singsongsangsong/service/song/SongServiceImpl.java
+++ backend/spring/SingSongSangSong/src/main/java/com/ssafy/singsongsangsong/service/song/SongServiceImpl.java
@@ -75,8 +75,8 @@ public class SongServiceImpl implements SongService {
 		// 기존 사용자가 해당 노래에 대해서 emotion을 남긴 경우, 해당 노래의 emotion을 삭제하고 count down 시킨 다음,
 		// 새로운 감정을 추가한다. count up++
 
-		Song song = songRepository.getSongByArtistIdAndSongId(songId, artistId)
-			.orElseThrow(() -> new IllegalArgumentException("해당 노래가 존재하지 않습니다."));
+		Song song = songRepository.findById(songId).orElseThrow(NotFoundSongException::new);
+
 		Optional<java.lang.String> targetEmotion = emotionRepository.checkIfEmotionExists(song.getId(), artistId);
 		if (targetEmotion.isPresent()) {
 			java.lang.String previousEmotionName = targetEmotion.get();
@@ -86,11 +86,8 @@ public class SongServiceImpl implements SongService {
 			songRepository.incrementEmotionCount(song.getId(), artistId, emotionType.getName());
 		} else {
 			// emotion 추가 및 반정규화된 table, count++
-			emotionRepository.save(Emotions.builder()
-				.song(song)
-				.artist(song.getArtist())
-				.emotionType(emotionType)
-				.build());
+			emotionRepository.save(
+				Emotions.builder().song(song).artist(song.getArtist()).emotionType(emotionType).build());
 			songRepository.incrementEmotionCount(song.getId(), artistId, emotionType.getName());
 		}
 	}
@@ -110,9 +107,7 @@ public class SongServiceImpl implements SongService {
 			.stream()
 			.map(CommentsResponse::from)
 			.toList();
-		return CommentsResponseDto.builder()
-			.comments(comments)
-			.build();
+		return CommentsResponseDto.builder().comments(comments).build();
 	}
 
 	@Override
@@ -132,8 +127,8 @@ public class SongServiceImpl implements SongService {
 
 		SongInfoResponseBuilder builder = SongInfoResponse.builder();
 
-		String musicFileName = Optional.ofNullable(song.getMusicFileName()).orElseGet(
-			DefaultFileName.DEFAULT_PROFILE_PICTURE::getName);
+		String musicFileName = Optional.ofNullable(song.getMusicFileName())
+			.orElseGet(DefaultFileName.DEFAULT_PROFILE_PICTURE::getName);
 
 		String originalFileName;
 		if (song.getAlbumImage() != null && song.getAlbumImage().getOriginalFileName() != null) {
@@ -142,19 +137,23 @@ public class SongServiceImpl implements SongService {
 			originalFileName = DefaultFileName.DEFAULT_PROFILE_PICTURE.getName();
 		}
 
-		builder = builder.songTitle(song.getTitle()).artist(ArtistInfoDto.from(artist))
-			.lyrics(song.getLyrics()).chord(song.getChord()).bpm(song.getBpm())
-			.songFileName(musicFileName).albumImageFileName(originalFileName)
+		builder = builder.songTitle(song.getTitle())
+			.artist(ArtistInfoDto.from(artist))
+			.lyrics(song.getLyrics())
+			.chord(song.getChord())
+			.bpm(song.getBpm())
+			.songFileName(musicFileName)
+			.albumImageFileName(originalFileName)
 			.songDescription(song.getSongDescription());
 
 		builder = builder.movedEmotionCount(song.getMovedEmotionCount())
-			.likeEmotionCount(song.getLikeEmotionCount()).energizedEmotionCount(song.getEnergizedEmotionCount())
+			.likeEmotionCount(song.getLikeEmotionCount())
+			.energizedEmotionCount(song.getEnergizedEmotionCount())
 			.excitedEmotionCount(song.getExcitedEmotionCount())
-			.funnyEmotionCount(song.getFunnyEmotionCount()).sadEmotionCount(song.getSadEmotionCount());
+			.funnyEmotionCount(song.getFunnyEmotionCount())
+			.sadEmotionCount(song.getSadEmotionCount());
 
-		List<CommentsInfoDto> comments = commentsList.stream()
-			.map(CommentsInfoDto::from)
-			.toList();
+		List<CommentsInfoDto> comments = commentsList.stream().map(CommentsInfoDto::from).toList();
 
 		builder = builder.likeCount(song.getLikeCount())
 			.downloadCount(song.getDownloadCount())
@@ -206,8 +205,7 @@ public class SongServiceImpl implements SongService {
 		}
 
 		Song song = songRepository.findById(songId).orElseThrow(NotFoundSongException::new);
-		List<SimilarityInfo> retrieved = webClientRequestService.requestSelectSimilarity(
-			songId);
+		List<SimilarityInfo> retrieved = webClientRequestService.requestSelectSimilarity(songId);
 		Collections.sort(retrieved);
 
 		List<Comparison> comparison = retrieved.stream().map(similarityInfo -> {
@@ -222,13 +220,14 @@ public class SongServiceImpl implements SongService {
 			}
 
 			return Comparison.builder()
-				.target(
-					Comparison.Target.builder()
-						.songId(targetSong.getId())
-						.albumImageFileName(originalFileName)
-						.title(targetSong.getTitle())
-						.createdDate(targetSong.getCreatedDate()).build()
-				).correlation(similarityInfo.getDistance()).build();
+				.target(Comparison.Target.builder()
+					.songId(targetSong.getId())
+					.albumImageFileName(originalFileName)
+					.title(targetSong.getTitle())
+					.createdDate(targetSong.getCreatedDate())
+					.build())
+				.correlation(similarityInfo.getDistance())
+				.build();
 		}).toList();
 
 		String originalAlbumImageFile;
